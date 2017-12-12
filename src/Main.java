401dc6b669a4d6fa896857e093898944a0113276
BestitzanosDimitris <32648577+BestitzanosDimitris@users.noreply.github.com>
2017-12-12 13:30:16 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
-	//o xrhsths h 8a dinei apo plhktrologio h ua dinei txt arxeio(einai kanonismeno) 
+	//eisagvgh ellhnikwn
+	//dhmiourgia 24 collection apo to txt tou lexikou + toLowercase()
 	
+	//TA EXCEPTIONS PAIDIAAA
+	public static void main(String[] args) throws InputMismatchException, FileNotFoundException, IOException {
+		int y = 2;
+		do {
+			System.out.printf("Press 1  to enter text from the keyboard \nPress 2 to enter txt file ");
+			Scanner scanner = new Scanner(System.in);
+			int x = scanner.nextInt();
+			
+			while(x!=1 && x!= 2) {
+				System.out.printf("Wrong input\n");
+				System.out.printf("Press 1  to enter text from the keyboard \nPress 2 to enter txt file ");
+				x = scanner.nextInt();
+			}
+
+			if (x == 1) {
+				UserInput.getText();
+			}
+			if (x == 2) { 
+				UserInput.getFile();
+			}
+			//edw ginontai ta pragmata
+			System.out.printf("Press 1 to exit \nPress 2 to continue");
+			y = scanner.nextInt();
+			while(y!=1 && y!= 2) {
+				System.out.printf("Wrong input\n");
+				System.out.printf("Press 1 to exit \nPress 2 to continue");
+				y = scanner.nextInt();
+			}
+		}while(y != 1);
+
+	}
+
 }
+
+
